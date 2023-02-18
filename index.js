ac069ee554c9db029724c714aed7f0241ee71c35
Winstein Martins <sriwinstein@gmail.com>
2023-02-18 06:23:34 -0300

    mask form inputs

--- index.js
+++ index.js
@@ -13,6 +13,46 @@ const cardMonth = document.querySelector('#card-month');
 const cardYear = document.querySelector('#card-year');
 const cardCVC = document.querySelector('#card-cvc');
 
+const onNameChange = (event) => {
+  const name = event.target.value;
+  cardName.textContent = name.length ? name : 'Jane Appleseed';
+};
+
+const onNumberChange = (event) => {
+  const number = event.target.value;
+  cardNumber.textContent = number.length ? number : '0000 0000 0000 0000';
+};
+
+const onMonthChange = (event) => {
+  if (event.target.value.length > 2) {
+    event.target.value = event.target.value.slice(0, 2);
+  }
+  const month = event.target.value;
+  cardMonth.textContent = month.length ? month : '00';
+};
+
+const onYearChange = (event) => {
+  if (event.target.value.length > 2) {
+    event.target.value = event.target.value.slice(0, 2);
+  }
+  const year = event.target.value;
+  cardYear.textContent = year.length ? year : '00';
+};
+
+const onCVCChange = (event) => {
+  if (event.target.value.length > 2) {
+    event.target.value = event.target.value.slice(0, 3);
+  }
+  const cvc = event.target.value;
+  cardCVC.textContent = cvc.length ? cvc : '000';
+};
+
+nameInput.addEventListener('input', onNameChange);
+numberInput.addEventListener('input', onNumberChange);
+monthInput.addEventListener('input', onMonthChange);
+yearInput.addEventListener('input', onYearChange);
+cvcInput.addEventListener('input', onCVCChange);
+
 const onSubmit = (event) => {
   event.preventDefault();
 
@@ -23,30 +63,16 @@ const onSubmit = (event) => {
   }
 };
 
-const onNameChange = (event) => {
-  cardName.textContent = event.target.value;
-};
-
-const onNumberChange = (event) => {
-  cardNumber.textContent = event.target.value;
-};
-
-const onMonthChange = (event) => {
-  cardMonth.textContent = event.target.value;
-};
-
-const onYearChange = (event) => {
-  cardYear.textContent = event.target.value;
-};
-
-const onCVCChange = (event) => {
-  cardCVC.textContent = event.target.value;
-};
-
 cardForm.addEventListener('submit', onSubmit);
 
-nameInput.addEventListener('input', onNameChange);
-numberInput.addEventListener('input', onNumberChange);
-monthInput.addEventListener('input', onMonthChange);
-yearInput.addEventListener('input', onYearChange);
-cvcInput.addEventListener('input', onCVCChange);
+const maskCardNumber = (event) => {
+  // Key pressed is not backspace or delete
+  if (event.keyCode != 46 && event.keyCode != 8) {
+    const i = numberInput.value.length;
+    if (i === 4 || i === 9 || i === 14) {
+      numberInput.value += ' ';
+    }
+  }
+};
+
+document.addEventListener('keydown', maskCardNumber);
