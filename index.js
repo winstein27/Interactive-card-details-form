5ef09a1846f9039402c2223eca24adedd9d6657c
Winstein Martins <sriwinstein@gmail.com>
2023-02-19 06:08:07 -0300

    validation styles

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Selectors
 const cardForm = document.querySelector('#card-form');
 const successScreen = document.querySelector('#success-screen');
 
@@ -13,6 +14,12 @@ const cardMonth = document.querySelector('#card-month');
 const cardYear = document.querySelector('#card-year');
 const cardCVC = document.querySelector('#card-cvc');
 
+const nameInfo = document.querySelector('#name-info');
+const numberInfo = document.querySelector('#number-info');
+const dateInfo = document.querySelector('#date-info');
+const cvcInfo = document.querySelector('#cvc-info');
+
+// Data binding
 const onNameChange = (event) => {
   const name = event.target.value;
   cardName.textContent = name.length ? name : 'Jane Appleseed';
@@ -53,6 +60,7 @@ monthInput.addEventListener('input', onMonthChange);
 yearInput.addEventListener('input', onYearChange);
 cvcInput.addEventListener('input', onCVCChange);
 
+// Mask
 const maskCardNumber = (event) => {
   // Key pressed is not backspace or delete
   if (event.keyCode != 46 && event.keyCode != 8) {
@@ -65,6 +73,68 @@ const maskCardNumber = (event) => {
 
 document.addEventListener('keydown', maskCardNumber);
 
+// Validation
+const genericValidation = (name, value, condition, input, info) => {
+  if (condition) {
+    input.style.borderColor = 'hsl(0, 100%, 66%)';
+    info.textContent = 'Invalid ' + name;
+
+    if (!value.length) {
+      info.textContent = "Can't be blank";
+    }
+  } else {
+    input.style.borderColor = 'hsl(270, 3%, 87%)';
+    info.textContent = '';
+  }
+};
+
+const nameValidate = (event) => {
+  const name = 'name';
+  const value = event.target.value;
+  const condition = value.length < 4 || !value.includes(' ');
+
+  genericValidation(name, value, condition, nameInput, nameInfo);
+};
+
+const numberValidate = (event) => {
+  const name = 'card number';
+  const value = event.target.value;
+  const condition = value.length !== 19 || !/^[0-9\s]*$/.test(value);
+
+  genericValidation(name, value, condition, numberInput, numberInfo);
+};
+
+const monthValidate = (event) => {
+  const name = 'month';
+  const value = event.target.value;
+  const condition = value < 1 || value > 12;
+
+  genericValidation(name, value, condition, monthInput, dateInfo);
+};
+
+const yearValidate = (event) => {
+  const name = 'year';
+  const value = event.target.value;
+  const condition = value < 23 || value > 30;
+
+  genericValidation(name, value, condition, yearInput, dateInfo);
+};
+
+const CVCValidate = (event) => {
+  const name = 'CVC';
+  const value = event.target.value;
+  const condition = value.length !== 3;
+
+  genericValidation(name, value, condition, cvcInput, cvcInfo);
+};
+
+nameInput.addEventListener('blur', nameValidate);
+numberInput.addEventListener('blur', numberValidate);
+monthInput.addEventListener('blur', monthValidate);
+yearInput.addEventListener('blur', yearValidate);
+cvcInput.addEventListener('blur', CVCValidate);
+
+// Submit
 const onSubmit = (event) => {
   event.preventDefault();
 
@@ -76,7 +146,7 @@ const onSubmit = (event) => {
   }
 
   const number = numberInput.value.trim();
-  if (number.length !== 19) {
+  if (number.length !== 19 || !/^[0-9\s]*$/.test(number)) {
     formIsValid = false;
   }
 
